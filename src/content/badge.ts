91de09915b59d784bfca51691d8e227a6b03fab5
da2el-ai <hirosyg.picasa@gmail.com>
2025-10-08 18:26:11 +0900

    feat: メタデータのある画像にバッジではなく枠線を付与するように変更

--- src/content/badge.ts
+++ src/content/badge.ts
@@ -7,27 +7,28 @@
  * メタデータバッジを作成
  */
 export function createBadge(imgElement: HTMLImageElement): void {
-  // 画像の親要素
-  const imgParent = imgElement.parentElement;
+  imgElement.classList.add('d2-meta-metaimage');
+  // // 画像の親要素
+  // const imgParent = imgElement.parentElement;
 
-  // 既存のバッジがあれば何もしない
-  if( imgParent && imgParent.querySelectorAll('.d2-meta-badge').length){
-    return;
-  }
+  // // 既存のバッジがあれば何もしない
+  // if( imgParent && imgParent.querySelectorAll('.d2-meta-badge').length){
+  //   return;
+  // }
   
-  // バッジ要素を作成
-  const badge = document.createElement('div');
-  badge.className = 'd2-meta-badge';
-  badge.textContent = 'META';
+  // // バッジ要素を作成
+  // const badge = document.createElement('div');
+  // badge.className = 'd2-meta-badge';
+  // badge.textContent = 'META';
   
-  // 画像の位置に合わせて配置
-  const rect = imgElement.getBoundingClientRect();
+  // // 画像の位置に合わせて配置
+  // const rect = imgElement.getBoundingClientRect();
   
-  if (imgParent) {
-    const computedStyle = window.getComputedStyle(imgParent);
-    if(!['relative', 'absolute', 'fixed'].includes(computedStyle.position)){
-      imgParent.style.position = 'relative';
-    }
-    imgParent.appendChild(badge);
-  }
+  // if (imgParent) {
+  //   const computedStyle = window.getComputedStyle(imgParent);
+  //   if(!['relative', 'absolute', 'fixed'].includes(computedStyle.position)){
+  //     imgParent.style.position = 'relative';
+  //   }
+  //   imgParent.appendChild(badge);
+  // }
 }
